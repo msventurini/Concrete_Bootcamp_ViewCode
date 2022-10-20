bafd9d56aa704b1f87c435f349137cf1a13491c0
msventurini <matheus.venturini@inf.ufrgs.br>
2022-10-19 22:37:29 -0300

    Initial Commit

---
+++ Concrete_Bootcamp_ViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Concrete_Bootcamp_ViewCode
+//
+//  Created by Matheus Silveira Venturini on 19/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Concrete_Bootcamp_ViewCodeUITests/Concrete_Bootcamp_ViewCodeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Concrete_Bootcamp_ViewCodeUITestsLaunchTests.swift
+//  Concrete_Bootcamp_ViewCodeUITests
+//
+//  Created by Matheus Silveira Venturini on 19/10/22.
+//
+
+import XCTest
+
+final class Concrete_Bootcamp_ViewCodeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
