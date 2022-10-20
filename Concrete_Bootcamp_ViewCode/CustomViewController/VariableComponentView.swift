0da483088a0f7cf05e9abd841eb04a1f10d45a0a
msventurini <matheus.venturini@inf.ufrgs.br>
2022-10-20 02:45:26 -0300

    Template de uma view nova.

---
+++ Concrete_Bootcamp_ViewCode/CustomViewController/VariableComponentView.swift
@@ -0,0 +1,20 @@
+//
+//  VariableComponentView.swift
+//  Concrete_Bootcamp_ViewCode
+//
+//  Created by Matheus Silveira Venturini on 20/10/22.
+//
+
+import UIKit
+
+class VariableComponentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
